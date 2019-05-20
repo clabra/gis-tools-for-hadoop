fc410055c8a38a06a7bc42667431b18fc8676552
CL <clabra@gmail.com>
2019-05-20 12:56:17 -0700

    Fix dir typos

--- create-dirs.sh
+++ create-dirs.sh
@@ -1,4 +1,3 @@
-hadoop fs -mkdir /user/cloudera/earthquake-demo/earthquake-data/
-hadoop fs -mkdir /user/cloudera/earthquake-demo/california-data/
-hadoop fs -put samples/data/earthquake-data/earthquakes.csv /user/cloudera/earthquake-demo/earthquake-data/
-hadoop fs -put samples/data/counties-data/california-counties.json /user/cloudera/earthquake-demo/california-data/
+hadoop fs -put samples/data/earthquake-data/earthquakes.csv /user/cloudera/earthquake-demo/earthquake-data/earthquakes.csv
+hadoop fs -put samples/data/counties-data/california-counties.json /user/cloudera/earthquake-demo/california-data/california-counties.json
+
