c6d170743dcc0b8a2c6b91906b81cef339704c04
CL <clabra@gmail.com>
2019-05-20 12:33:50 -0700

    Shell create dirs

---
+++ create-dirs.sh
@@ -0,0 +1,4 @@
+hadoop fs -mkdir /user/cloudera/earthquake-demo/earthquake-data/
+hadoop fs -mkdir /user/cloudera/earthquake-demo/california-counties/
+hadoop fs -put samples/data/earthquake-data/earthquakes.csv /user/cloudera/earthquake-demo/earthquake-data/
+hadoop fs -put samples/data/counties-data/california-counties.json /user/cloudera/earthquake-demo/california-counties/
